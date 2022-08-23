fb4208e0c2e4a51ad02a91e59186e365f6c2f792
Andrew Douglas <fairtomiddlingster@gmail.com>
2022-08-23 12:57:18 +0100

    shelved for the time being - couldnt get migrations to do much, happy with it so far tho

--- Model/TaxonomyDbContext.cs
+++ Model/TaxonomyDbContext.cs
@@ -19,7 +19,7 @@ namespace Model
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseMySql("Server=localhost;Uid=testuser;Pwd=jimbob;SslMode=Required", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.30-mysql"));
+                optionsBuilder.UseMySql("<>", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.30-mysql"));
             }
         }
 
